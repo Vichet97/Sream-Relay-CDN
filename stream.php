418524bbc7b972a816cd9f286cecd2b53b09e548
Vichet97 <hengsarathvichet@gmail.com>
2019-10-28 00:00:28 +0700

    dddd

--- stream.php
+++ stream.php
@@ -108,7 +108,7 @@ function getUrlData($url,$returnheader = false,$return = true ,$useragent=false,
     $stream = substr( str_replace(' ', '', $url) , 0, 4 ) === "http" ? explode('://', $url, 2)[1] : $url;
     $proxy = 'https://as.mykhcdn.workers.dev/cdn/';//encrypt_decrypt('decrypt',getParam("p")) ;   //https://as.mykhcdn.workers.dev/cdn/:uri
     $url = $proxy.rawurldecode($stream);   // https://as.mykhcdn.workers.dev/cdn/:encoded_uri
-    // echo $url;exit;
+    echo $url;exit;
 
     if(getParam("useragent")!=false)
     {
