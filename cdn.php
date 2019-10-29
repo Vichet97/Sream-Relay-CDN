e4974ae0a78dbb25b399d44c8c259282309eeae4
Vichet97 <hengsarathvichet@gmail.com>
2019-10-29 23:57:54 +0700

    d

--- cdn.php
+++ cdn.php
@@ -508,7 +508,7 @@ function getChunkURI()
         {
             $script = basename(__FILE__);
         }
-        $ts = explode("/", explode($script,curPageURL())[1])[1];
+        $ts = "iduri=".explode("/", explode("iduri=", $_SERVER['REQUEST_URI'])[1], 2)[0];
         echo $ts; exit;
         $tmp = substr(explode($ts, curPageURL())[1], 1);
         $tmp = str_replace("?authenticationtoken=","authenticationtoken=",$tmp);
