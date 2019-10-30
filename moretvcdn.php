13064055752718095d70a0b9ba2650a209dbe74d
Vichet97 <hengsarathvichet@gmail.com>
2019-10-30 10:46:27 +0700

    Update moretvcdn.php

--- moretvcdn.php
+++ moretvcdn.php
@@ -2,7 +2,7 @@
 $options = array(
   'http'=>array(
     'method'=>"GET",
-    'header'=>"Origin: pili-live-hdl.qhmywl.com\r\n" .
+    'header'=>"Origin: example.com\r\n" .
               "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
   )
 );
@@ -12,13 +12,12 @@ $context = stream_context_create($options);
 // Open the file using the HTTP headers set above
 
 $id = $_GET['id'];
-$uri = "http://pili-live-hdl.qhmywl.com/dsdtv/".$id.".m3u8";
+$uri = "http://example.com/".$id.".m3u8";
 try{
     if (empty($id) || !isset($id)) {
       throw new Exception("The field is undefined."); 
     }
     $proxy1 = "http://cors-any.fotor.com.cn/";
-    $proxy2 = "http://cors.awebman.com/";
     $file = @file_get_contents($proxy1.$uri, false, $context);
 
     if (getHttpCode($http_response_header) != 200) {
